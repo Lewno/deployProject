028384842444fc5c40716ba8746840a193cca9cb
lewno <cabax.2002@gmail.com>
2023-08-24 20:29:13 -0300

    prueba4

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import logo from "../assets/images/logo.png"
+import { Link } from "react-router-dom"
 const Navbar = () => {
   return (
     <nav className='w-full flex p-3 justify-between items-center'>
       <img src={logo} alt="" className='w-[5rem]' />
-      <button className=' bg-orange-400 p-1 px-4 rounded-3xl text-sm'>INICIAR SESIÓN</button>
+      <Link to={"/login"}>
+        <button className=' bg-orange-400 p-1 px-4 rounded-3xl text-sm'>INICIAR SESIÓN</button>
+      </Link>
     </nav>
   )
 }
